ee9f9200521b581b22fae07f0da02bb8392fbdd8
Lucas BRaga <lucasrbraga77@gmail.com>
2025-03-18 12:57:17 -0300

    Adiciona funcionalidades de cadastro de conta no ContaTerminal.java
    - Adiciona entradas para salvar número da conta, agência, nome do cliente e saldo do cliente.
    - Implementa validação do formato da agência no padrão "XXX-9".
    - Exibe mensagem de agradecimento ao usuário com os dados cadastrados.
    - Adiciona o arquivo ContaTerminal.class gerado após a compilação.
    
    Agora, o usuário pode cadastrar sua conta com as informações necessárias e receber uma mensagem personalizada de agradecimento.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,64 @@
-public class ContaTerminal {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class ContaTerminal {
+    public String agencia = "";
+    public String nomeCliente = "";
+    public double saldo = 0.0;
+    public int numeroDaConta = 0;
+    public static void main(String[] args) {
+        try {
+           Scanner input = new Scanner(System.in);
+            ContaTerminal conta = new ContaTerminal();
+            conta.NumeroConta(input);
+            conta.Agencia(input);
+            conta.NomeCliente(input);
+            conta.SaldoCliente(input);
+            System.out.println("Olá " + conta.nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + conta.agencia + ", conta " + conta.numeroDaConta + " e seu saldo "+ conta.saldo +" já está disponível para saque.");
+        }catch (InputMismatchException e){
+
+        }
+    }
+    public int NumeroConta(Scanner scanner){
+        try{
+            System.out.println("Por favor,digite o numero de da Agencia:");
+            this.numeroDaConta = scanner.nextInt();
+        }catch(InputMismatchException e){
+            System.out.println("ERRO: Tipo de dado incorreto. Por favor tente novamente.");
+            scanner.nextLine();
+            this.NumeroConta(scanner);
+        }
+        return this.numeroDaConta;
+    }
+    public String Agencia(Scanner scanner){
+        try{
+            System.out.println("Por favor,digite a agencia:");
+            this.agencia = scanner.next();
+        }catch(InputMismatchException e){
+            System.out.println("ERRO: Tipo de dado incorreto. Por favor tente novamente.");
+            this.Agencia(scanner);
+        }
+        return this.agencia;
+    }
+    public String NomeCliente(Scanner scanner){
+        try{
+            System.out.println("Por favor,digite o nome do cliente:");
+            this.nomeCliente = scanner.next();
+        }catch(InputMismatchException e){
+            System.out.println("ERRO: Tipo de dado incorreto. Por favor tente novamente.");
+            this.NomeCliente(scanner);
+        }
+        return this.nomeCliente;
+    }
+    public double SaldoCliente(Scanner scanner){
+        try{
+            System.out.println("Por favor,digite o saldo do cliente " + this.nomeCliente+":");
+            this.saldo = scanner.nextDouble();
+        }catch(InputMismatchException e){
+            System.out.println("ERRO: Tipo de dado incorreto. Por favor tente novamente.");
+            scanner.nextLine();
+            this.SaldoCliente(scanner);
+        }
+        return this.saldo;
     }
 }
